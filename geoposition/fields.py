c67e659b9cfbfb7f809823f4ac30e75ca59020c9
Luke Pomeroy <lukepomeroy@hotmail.co.uk>
2022-06-28 19:50:39 +0100

    Removed references to django.utils.six and with_metaclass.

--- geoposition/fields.py
+++ geoposition/fields.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.utils.six import with_metaclass
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import smart_text
 
@@ -40,19 +39,18 @@ class GeopositionField(models.Field):
 
         return Geoposition(latitude, longitude)
 
-    def from_db_value(self, value, expression, connection, context):
+    # TODO: drop context parameter when dropping 1.11 support
+    def from_db_value(self, value, expression, connection, context=None):
         return self.to_python(value)
 
     def get_prep_value(self, value):
         return str(value)
 
     def value_to_string(self, obj):
-        value = self._get_val_from_obj(obj)
+        value = self.value_from_object(obj)
         return smart_text(value)
 
     def formfield(self, **kwargs):
-        defaults = {
-            'form_class': GeopositionFormField
-        }
+        defaults = {'form_class': GeopositionFormField}
         defaults.update(kwargs)
         return super(GeopositionField, self).formfield(**defaults)
--- geoposition/widgets.py
+++ geoposition/widgets.py
@@ -1,36 +1,74 @@
 from __future__ import unicode_literals
 
 import json
+import sys
+
 from django import forms
-from django.template.loader import render_to_string
-from django.utils import six
 from django.utils.translation import ugettext_lazy as _
+
 from .conf import settings
 
 
+PY2 = sys.version_info[0] == 2
+if PY2:
+    text_type = unicode
+else:
+    text_type = str
+
+
 class GeopositionWidget(forms.MultiWidget):
+    template_name = 'geoposition/widgets/geoposition.html'
+
+    backends = {
+        'google': {
+            'js': (
+                '//maps.google.com/maps/api/js?key=%s' % settings.GOOGLE_MAPS_API_KEY,
+                'geoposition/google.js',
+            ),
+            'css': (),
+        },
+        'leaflet': {
+            'js': (
+                '//unpkg.com/leaflet@1.2.0/dist/leaflet.js',
+                '//unpkg.com/leaflet-control-geocoder@1.5.6/dist/Control.Geocoder.js',
+                'geoposition/leaflet.js',
+            ),
+            'css': (
+                '//unpkg.com/leaflet@1.2.0/dist/leaflet.css',
+                '//unpkg.com/leaflet-control-geocoder@1.5.6/dist/Control.Geocoder.css',
+            ),
+        }
+    }
+
     def __init__(self, attrs=None):
+        self.Media.js = self.backends[settings.BACKEND]['js']
+        self.Media.css['all'] = self.backends[settings.BACKEND]['css'] + self.Media.css['all']
         widgets = (
             forms.TextInput(),
             forms.TextInput(),
         )
         super(GeopositionWidget, self).__init__(widgets, attrs)
 
-    def decompress(self, value):
-        if isinstance(value, six.text_type):
-            return value.rsplit(',')
-        if value:
-            return [value.latitude, value.longitude]
-        return [None, None]
+    def get_config(self):
+        return {
+            'map_widget_height': settings.MAP_WIDGET_HEIGHT or 500,
+            'map_options': json.dumps(settings.MAP_OPTIONS),
+            'marker_options': json.dumps(settings.MARKER_OPTIONS),
+        }
 
-    def format_output(self, rendered_widgets):
-        return render_to_string('geoposition/widgets/geoposition.html', {
+    def get_context(self, name, value, attrs):
+        context = super(GeopositionWidget, self).get_context(name, value, attrs)
+        if not isinstance(value, list):
+            value = self.decompress(value)
+        if 'widget' not in context:
+            context['widget'] = {}
+        context['widget'].update({
             'latitude': {
-                'html': rendered_widgets[0],
+                'html': value[0],
                 'label': _("latitude"),
             },
             'longitude': {
-                'html': rendered_widgets[1],
+                'html': value[1],
                 'label': _("longitude"),
             },
             'config': {
@@ -39,12 +77,16 @@ class GeopositionWidget(forms.MultiWidget):
                 'marker_options': json.dumps(settings.MARKER_OPTIONS),
             }
         })
+        return context
+
+    def decompress(self, value):
+        if isinstance(value, text_type):
+            return value.rsplit(',')
+        if value:
+            return [value.latitude, value.longitude]
+        return [None, None]
 
     class Media:
-        js = (
-            '//maps.google.com/maps/api/js?key=%s' % settings.GOOGLE_MAPS_API_KEY,
-            'geoposition/geoposition.js',
-        )
         css = {
             'all': ('geoposition/geoposition.css',)
         }
